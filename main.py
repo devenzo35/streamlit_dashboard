96e42ccd24be5d9fe02e5f349a5c6fc3e8f6437a
Deevenzo35 <enzocuellar12@gmail.com>
2025-09-29 17:12:12 -0300

    building sidebar and select box to render different info

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import streamlit as st
+import pandas as pd
+from pandas import DataFrame
+from typing import Union
+
+
 def main():
-    print("Hello from Streamlit Dashboard")
+    st.title("Finance Dashboard")
+
+    st.divider()
+
+    st.sidebar.title("Options")
+    options = st.sidebar.selectbox("Select", options=["Pattern", "Tweet", "StockTwits"])
+
+    if options == "Pattern":
+        st.text("You just selected pattern page")
+        st.image(
+            "https://eq-cdn.equiti-me.com/website/images/Artwork_of_different_chart_patterns_for_Chart_.width-800.jpg"
+        )
+
+    if options == "Tweet":
+        st.text("You just selected Tweet page")
+        st.image("https://cdn.mos.cms.futurecdn.net/z3bn6deaxmrjmQHNEkpcZE.jpg")
+
+    if options == "StockTwits":
+        st.image("https://upload.wikimedia.org/wikipedia/en/5/58/Stocktiwts.png")
 
 
 if __name__ == "__main__":
