053418918d0baa8b338f7fda53a5f3a7e5b8101a
Deevenzo35 <enzocuellar12@gmail.com>
2025-10-01 18:41:18 -0300

    added new options Chart Viewer, Trading journal, added DB Connection

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import streamlit as st
-import praw
 import psycopg2
-from dotenv import load_dotenv
+import psycopg2.extras
 import os
+import praw  # type: ignore
+import pandas as pd
+from typing import Union
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
-connection = psycopg2.connect(
+conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
 )
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
@@ -27,14 +32,39 @@ def main():
 
     st.sidebar.title("Options")
     options = st.sidebar.selectbox(
-        "Select", options=["Pattern", "Reddit", "StockTwits"]
+        "Select", ["Chart Viewer", "Trading Journal", "Reddit", "StockTwits"], index=0
     )
 
-    if options == "Pattern":
-        st.text("You just selected pattern page")
-        st.image(
-            "https://eq-cdn.equiti-me.com/website/images/Artwork_of_different_chart_patterns_for_Chart_.width-800.jpg"
-        )
+    if options == "Chart Viewer":
+        symbol = st.text_input(label="Search symbol", max_chars=5, placeholder="AAPL")
+        if symbol:
+            st.image(f"https://finviz.com/chart.ashx?t={symbol}")
+
+    if options == "Trading Journal":
+        query_portfolio = """
+            Select s.symbol, SUM(CASE When t.action = 'BUY' then t.quantity else -t.quantity END) as net_quantity,
+            (SELECT close FROM prices p Where t.stock_id = p.stock_id LIMIT 1) as last_price
+            from trades t
+            JOIN stocks s 
+            ON s.id = t.stock_id
+            GROUP BY s.symbol, t.stock_id
+            """
+
+        cursor.execute(query_portfolio)
+
+        counts = cursor.fetchall()
+
+        stock_list: list[dict[str, Union[str, int]]] = [
+            {
+                "Symbol": c[0],
+                "Quantity": c[1],
+                "Last_Price": c[2],
+            }
+            for c in counts
+        ]
+
+        pf = pd.DataFrame(stock_list)
+        st.write(pf)
 
     if options == "Reddit":
         selected_subreddit = st.sidebar.selectbox(
